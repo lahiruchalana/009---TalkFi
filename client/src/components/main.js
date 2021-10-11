307eca35ad45ccf07021467a6553d95e835d9049
W.D. Lahiru Chalana Iresh <55834083+lahiruchalana@users.noreply.github.com>
2021-10-11 09:39:17 +0530

    added express.js backend and set environment for new google api keys and tested speech-to-text api successfully in speechToText.js file

---
+++ client/src/components/main.js
@@ -0,0 +1,53 @@
+import React from 'react';
+
+// import music from './../../resources/audio.raw'
+
+// import fs from 'fs';
+// import speech from '@google-cloud/speech';
+
+
+const main = () => {
+    
+    // // Creates a client
+    // const client = new speech.SpeechClient();
+
+    // const filename = './resources/audio3.raw';
+    // const encoding = 'LINEAR16';
+    // const sampleRateHertz = 16000;
+    // const languageCode = 'en-US';
+
+    // const config = {
+    // encoding: encoding,
+    // sampleRateHertz: sampleRateHertz,
+    // languageCode: languageCode,
+    // };
+    // const audio = {
+    // content: fs.readFileSync(filename).toString('base64'),
+    // };
+
+    // const request = {
+    // config: config,
+    // audio: audio,
+    // };
+
+    // async function handling() {
+    //     const [operation] = await client.longRunningRecognize(request);
+    //     const [response] = await operation.promise();
+    //     const transcription = response.results
+    //     .map(result => result.alternatives[0].transcript)
+    //     .join('\n');
+    //     console.log(`Transcription: ${transcription}`);
+    // }
+    
+    // handling()
+
+    return ( 
+        <div>
+            <button>Hello</button>
+        </div>
+    );
+}    
+
+export default main;
+
+
